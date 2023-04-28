0456a2aa5c4933163514b0ea1cfe43f29de54514
Ismael Bueno <ismaelwno@gmail.com>
2023-04-29 01:14:52 +0200

    Added pch files

---
+++ include/ini/pch.hpp
@@ -0,0 +1,13 @@
+#ifndef INI_PCH_HPP
+#define INI_PCH_HPP
+
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+#include <iomanip>
+#include <limits>
+#include <string>
+#include <vector>
+
+#endif
